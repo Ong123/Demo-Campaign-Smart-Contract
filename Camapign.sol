8e87c18a2842011ba3012b37552ceda10df54b46
Ong Tshering Lepcha <36501322+Ong123@users.noreply.github.com>
2022-03-16 18:20:04 +0530

    Update Camapign.sol

--- Camapign.sol
+++ Camapign.sol
@@ -1,15 +1,48 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-contract CampaignFactory {
+contract CloneFactory {
+
+  function createClone(address target) internal returns (address result) {
+    bytes20 targetBytes = bytes20(target);
+    assembly {
+      let clone := mload(0x40)
+      mstore(clone, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
+      mstore(add(clone, 0x14), targetBytes)
+      mstore(add(clone, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
+      result := create(0, clone, 0x37)
+    }
+  }
+
+  function isClone(address target, address query) internal view returns (bool result) {
+    bytes20 targetBytes = bytes20(target);
+    assembly {
+      let clone := mload(0x40)
+      mstore(clone, 0x363d3d373d3d3d363d7300000000000000000000000000000000000000000000)
+      mstore(add(clone, 0xa), targetBytes)
+      mstore(add(clone, 0x1e), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
+
+      let other := add(clone, 0x40)
+      extcodecopy(query, other, 0, 0x2d)
+      result := and(
+        eq(mload(clone), mload(other)),
+        eq(mload(add(clone, 0xd)), mload(add(other, 0xd)))
+      )
+    }
+  }
+}
+
+contract CampaignFactory is CloneFactory {
     address public _campaignManager;
     Campaign[] public campaignConducted;
+    address public campaignContractAddress;
 
     event CampaignEvent(address _admin, Campaign _campaign);
     
 
-    constructor(){
+    constructor(address _campaignContractAddress){
         _campaignManager = msg.sender;
+        campaignContractAddress = _campaignContractAddress;
     }
 
     modifier onlyCampaignManager() {
@@ -21,13 +54,14 @@ contract CampaignFactory {
         return msg.sender == _campaignManager;
     }
     
-    function createCampaign (uint _minimum) public onlyCampaignManager {
-        Campaign campaign = new Campaign(_minimum ,payable(msg.sender));
+   
+    function createCampaign(uint _minimum) public onlyCampaignManager {
+        Campaign campaign = Campaign(createClone(campaignContractAddress));
+        campaign.initialize(_minimum, payable(msg.sender));
         campaignConducted.push(campaign);
-        emit CampaignEvent(msg.sender,campaign);
     }
     
-    function getElections() public view returns(Campaign[] memory _campaignConducted) {
+    function getCampaign() public view returns(Campaign[] memory _campaignConducted) {
         return campaignConducted;
     }    
 
@@ -68,7 +102,7 @@ contract Campaign {
         _;
     }
 
-    constructor(uint _minimum, address payable _creator) public {
+    function initialize( uint _minimum, address payable _creator) external {
         manager = _creator;
         minimumContribution = _minimum;
     }
